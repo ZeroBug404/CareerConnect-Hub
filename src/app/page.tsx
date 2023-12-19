853c554b3bb1b4aee8de37d3ba237115a507954f
Rabbi Reza <rabbi.cse1.bu@gmail.com>
2023-12-19 20:01:04 +0600

    Update footer file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-import Header from "@/components/ui/Header";
-import Image from "next/image";
-import styles from "./page.module.css";
-import Footer from "@/components/ui/Footer";
-
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <main>
-        Here The main body
-      </main>
-      <Footer />
-    </>
-  );
+  return <main>Here The main body</main>;
 }
--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,5 +1,29 @@
+import styles from "@/components/ui/footer.module.css";
+import { Button } from "antd";
+
 const Footer = () => {
-  return <div>This is Footer</div>;
+  return (
+    <div className={styles.footer_div_main}>
+      <div className={styles.top_section}>
+        <div className={styles.left_section}>
+          <h2>We'Re here to help</h2>
+          <p>
+            Visit our Help Center for answers to common questions or contact us
+            directly.
+          </p>
+          <div>
+            <a href="" className={styles.outline_button}>Help center</a>
+          </div>
+        </div>
+        <div className={styles.right_section}>
+          <div>Indeed</div>
+          <div>Employers</div>
+          <div>Resources</div>
+        </div>
+      </div>
+      <div className={styles.bottom_section}></div>
+    </div>
+  );
 };
 
 export default Footer;
