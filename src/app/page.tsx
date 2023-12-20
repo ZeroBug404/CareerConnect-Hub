006a40e572a0af3678618909ad2eee70da8de00f
Shimu Mallick <404.zerobug@gmail.com>
2023-12-20 11:46:35 +0600

    Changes done for merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
 import HelpCenter from "@/components/ui/HelpCenter";
 
-import HomeBlog from "@/components/ui/HomeBlog";
-import HomePageBanner from "@/components/ui/HomePageBanner";
-import Overview from "@/components/ui/Overview";
-
 export default function Home() {
-  return (
-    <main>
-      {/* <Navbar></Navbar> */}
-      <HomePageBanner></HomePageBanner>
-      <Overview></Overview>
-      <HomeBlog></HomeBlog>
-      <HelpCenter />
-    </main>
-  );
+  return <main>
+    <HelpCenter />
+  </main>;
 }
+
+
