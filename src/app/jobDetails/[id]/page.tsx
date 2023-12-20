e3bccf1363257100993c466961e35319525e9496
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-12-20 15:14:07 +0600

    updated job details params type

--- src/app/jobDetails/[id]/page.tsx
+++ src/app/jobDetails/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { Button, Card } from "antd";
 
-const JobDetails = ({ params }) => {
+const JobDetails = ({ params }: any) => {
   return (
     <div
       style={{
@@ -9,7 +9,7 @@ const JobDetails = ({ params }) => {
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
-        margin: "0 20px", 
+        margin: "0 20px",
       }}
     >
       <Card
@@ -20,21 +20,22 @@ const JobDetails = ({ params }) => {
         }}
       >
         <p>
-          Years of exp. – 2-4 Yrs. <br /> Work Location –
-          Remote <br /> Number of Vacancies – 5 <br /> Key Skills – Technologies: HTML5, CSS3,
-          JavaScript, JQuery, Angular JS, Node JS, Backbone JS, Bootstrap, GitHub <br /> Type: Full Time | Remote <br />
+          Years of exp. – 2-4 Yrs. <br /> Work Location – Remote <br /> Number
+          of Vacancies – 5 <br /> Key Skills – Technologies: HTML5, CSS3,
+          JavaScript, JQuery, Angular JS, Node JS, Backbone JS, Bootstrap,
+          GitHub <br /> Type: Full Time | Remote <br />
           Experience & Responsibilities – At least 18 months of web development
-          experience using the tools mentioned above. Familiar with SDLC and Agile
-          delivery process. Experienced in working in a team environment and
-          ability to collaborate with multiple team members. Strong proponent of
-          engineering best practices for Unit Testing, Coverage, Code Quality,
-          Continuous Integration & Continuous Deployment. Hands-on in development,
-          Self-starter with the ability to appropriately prioritize and plan
-          complex work in a rapidly changing environment. Excellent communication
-          skills with the ability to explain technical concepts in simple terms.
-          Some retail or e-commerce domain project experience will be a plus.
-          Qualifications – B.E. / B.Tech / MCA from a recognized university Job
-          
+          experience using the tools mentioned above. Familiar with SDLC and
+          Agile delivery process. Experienced in working in a team environment
+          and ability to collaborate with multiple team members. Strong
+          proponent of engineering best practices for Unit Testing, Coverage,
+          Code Quality, Continuous Integration & Continuous Deployment. Hands-on
+          in development, Self-starter with the ability to appropriately
+          prioritize and plan complex work in a rapidly changing environment.
+          Excellent communication skills with the ability to explain technical
+          concepts in simple terms. Some retail or e-commerce domain project
+          experience will be a plus. Qualifications – B.E. / B.Tech / MCA from a
+          recognized university Job
         </p>
         <Button type="primary">Apply Here</Button>
       </Card>
