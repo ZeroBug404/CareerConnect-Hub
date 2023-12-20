43b73ecb88383e4f5bdaa89a9bf1f4a905781a72
Shimu Mallick <404.zerobug@gmail.com>
2023-12-20 11:22:57 +0600

    Update global CSS and add components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 import HelpCenter from "@/components/ui/HelpCenter";
 
+import HomeBlog from "@/components/ui/HomeBlog";
+import HomePageBanner from "@/components/ui/HomePageBanner";
+import Overview from "@/components/ui/Overview";
+
 export default function Home() {
-  return <main>
-    <HelpCenter />
-  </main>;
+  return (
+    <main>
+      {/* <Navbar></Navbar> */}
+      <HomePageBanner></HomePageBanner>
+      <Overview></Overview>
+      <HomeBlog></HomeBlog>
+      <HelpCenter />
+    </main>
+  );
 }
