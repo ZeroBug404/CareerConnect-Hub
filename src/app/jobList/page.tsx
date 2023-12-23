ea2e4a71d86df5786be86f25bef2059d3f567d3b
Rabbi-Reza <rabbi.cse1.bu@gmail.com>
2023-12-23 12:56:07 +0600

    Job listing scroll

--- src/app/jobList/page.tsx
+++ src/app/jobList/page.tsx
@@ -1,43 +1,10 @@
-/* eslint-disable @next/next/no-async-client-component */
-"use client";
-
-import styles from "@/Styles/JobList.module.css";
-import JobDetailView from "@/components/ui/JobDetailView";
-import JobLists from "@/components/ui/JobLists";
-import { useState } from "react";
-import Search from "antd/es/input/Search";
+import JobListMain from "@/components/ui/JobListMain";
 
 const JobList = () => {
-  const [selectedID, setSelectedID] = useState(0);
   return (
-    <div style={{
-        marginBottom: "2vw",
-      }}>
-        <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-          marginTop: "16px",
-        }}
-      >
-        <Search
-          placeholder="Search for jobs"
-          enterButton="Search"
-          size="large"
-          style={{
-            maxWidth: "500px",
-            width: "100%",
-          }}
-        />
-      </div>
-      <div className={styles.JobList_div_main}>
-        <JobLists setSelectedID={setSelectedID} />
-        <JobDetailView selectedID={selectedID} />
-      </div>
-    </div>
+    <>
+      <JobListMain />
+    </>
   );
 };
 
