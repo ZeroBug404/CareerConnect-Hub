56422a1720ad00e66645b8134a597ec5d2e76354
Rabbi-Reza <rabbi.cse1.bu@gmail.com>
2023-12-23 12:26:35 +0600

    Few modification

--- src/app/jobList/page.tsx
+++ src/app/jobList/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+/* eslint-disable @next/next/no-async-client-component */
+"use client";
 
-const jobList = () => {
+import styles from "@/Styles/JobList.module.css";
+import JobDetailView from "@/components/ui/JobDetailView";
+import JobLists from "@/components/ui/JobLists";
+import { useState } from "react";
+import Search from "antd/es/input/Search";
+
+const JobList = () => {
+  const [selectedID, setSelectedID] = useState(0);
   return (
-    <div>page</div>
-  )
-}
+    <div>
+        <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          marginTop: "16px",
+        }}
+      >
+        <Search
+          placeholder="Search for jobs"
+          enterButton="Search"
+          size="large"
+          style={{
+            maxWidth: "500px",
+            width: "100%",
+          }}
+        />
+      </div>
+      <div className={styles.JobList_div_main}>
+        <JobLists setSelectedID={setSelectedID} />
+        <JobDetailView selectedID={selectedID} />
+      </div>
+    </div>
+  );
+};
 
-export default jobList
+export default JobList;
---
+++ src/components/ui/JobLists.tsx
@@ -0,0 +1,86 @@
+/* eslint-disable @next/next/no-async-client-component */
+"use client";
+
+import styles from "@/Styles/JobList.module.css";
+import { Card, Col, Flex, Row } from "antd";
+import Title from "antd/es/typography/Title";
+import { useEffect, useState } from "react";
+
+const JobLists = async (props: any) => {
+  const { setSelectedID } = props;
+
+  const [data, setData] = useState([]);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  useEffect(() => {
+    fetch("https://career-connect-hub-api.vercel.app/api/v1/jobs")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      });
+  }, []);
+
+  const onClickHandler = (index: any, ID: any): void => {
+    setSelectedIndex(index);
+    setSelectedID(ID);
+  };
+
+  return (
+    <div style={{ padding: "16px" }} className={styles.JobDetailView_div_main}>
+      <Row gutter={[16, 24]}>
+        {data?.map((job: any, index: number) => (
+          <Col
+            xs={24}
+            sm={24}
+            md={24}
+            lg={24}
+            key={job?._id}
+            onClick={() => onClickHandler(index, job._id)}
+          >
+            <div
+              style={{
+                width: "100%",
+                height: "100%",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                border: `${selectedIndex === index ? "2px solid #104278" : ""}`,
+                borderRadius: "10px",
+              }}
+            >
+              {/* <div
+                style={{
+                  padding: "10px",
+                  color: "blue",
+                }}
+              >
+                <Flex wrap="wrap" gap="small">
+                  <RiseOutlined /> <p>Active Hiring</p>
+                </Flex>
+              </div> */}
+              <Card
+                title={<Title level={4}>{job.title}</Title>}
+                bordered={false}
+              >
+                <h4>{job.company}</h4>
+                <br />
+                <Flex wrap="wrap" gap="small">
+                  <p>Location: {job.location},</p>
+                  <p>JobType: {job.jobType},</p>
+                  <p> Joining Date: {job.joiningDate},</p>
+                  <p>CTC: {job.salary},</p>
+                  <p>Experience: {job.experienceLevel}</p>
+                </Flex>
+                {/* <br />
+                <Flex wrap="wrap" gap="small" justify="end" align="center">
+                  <Link href={`/jobDetails/${job._id}`}>View Details</Link>
+                  <Button type="primary">Apply Now</Button>
+                </Flex> */}
+              </Card>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+export default JobLists;
