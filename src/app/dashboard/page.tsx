3459e854ad971f42d2957ef00723e73f45868aa9
Rabbi-Reza <rabbi.cse1.bu@gmail.com>
2023-12-24 15:26:19 +0600

    Dashboard Authenticate

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,26 +1,12 @@
 /* eslint-disable @next/next/no-async-client-component */
 "use client";
 
-import { getUserInfo } from "@/services/auth.service";
-import { useEffect, useState } from "react";
-
 const Dashboard = async () => {
-  const [role, setRole] = useState("");
-  
-  useEffect(() => {
-    const res = getUserInfo();
-    setRole(res?.role);
-  }, []);
+
 
   return (
     <div>
-      {role == "user" ? (
-        <>Job Seeker</>
-      ) : role == "admin" ? (
-        <>Admin</>
-      ) : (
-        <></>
-      )}
+      
     </div>
   );
 };
