6260d3a51b7daa17995ea0483bafcce3199fa2d7
shapna28 <shapna088@gmail.com>
2023-12-25 15:49:20 +0600

    create a reusetable

--- src/app/dashboard/job/page.tsx
+++ src/app/dashboard/job/page.tsx
@@ -1,14 +1,41 @@
+import CareerTable from '@/components/ui/CareerTable';
 import { Button } from 'antd';
 import Link from 'next/link';
 import React from 'react';
 
 const JobPage = () => {
+    const columns = [
+        {
+          title: "Title",
+          dataIndex: "title",
+        },
+        {
+          title: "Company",
+          dataIndex: "company",
+        },
+        {
+          title: "Location",
+          dataIndex: "location",
+        },
+        {
+          title: "Salary",
+          dataIndex: "salary",
+        },
+        {
+          title: "Job Type",
+          dataIndex: "jobType",
+        },
+      ];
     return (
         <div>
-            <h3>This job page</h3>
             <Link href="/dashboard/job/create">
                 <Button type="primary">Publish a job?</Button>
             </Link>
+            <CareerTable 
+            columns={columns}
+            showSizeChanger={true}
+            showPagination={true}
+            />
         </div>
     );
 };
