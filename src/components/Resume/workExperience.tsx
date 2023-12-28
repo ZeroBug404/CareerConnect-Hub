ed98ae80ebbb7875c39ab6d9a642b631a39add2b
maruf1996 <maruf3813@gmail.com>
2023-12-28 10:19:07 +0600

    add-education-experience-modal

--- src/components/Resume/workExperience.tsx
+++ src/components/Resume/workExperience.tsx
@@ -1,50 +1,52 @@
 import { DeleteOutlined, EditOutlined, PlusOutlined } from "@ant-design/icons";
 import { Button, Flex } from "antd";
+import { useState } from "react";
+import GlobalModal from "../Shared/GlobalModal";
+import ExperienceModal from "../ui/ExperienceModal";
 
 const WorkExperience = () => {
+  const [open, setOpen] = useState(false);
   return (
-      <Flex
-        wrap="wrap"
-        gap="small"
-        justify="space-between"
-        style={{
-          borderBottom: "1px solid black",
-          padding: "10px 0",
-        }}
-      >
+    <Flex
+      wrap="wrap"
+      gap="small"
+      justify="space-between"
+      style={{
+        borderBottom: "1px solid black",
+        padding: "10px 0",
+      }}
+    >
+      <div>
+        <p>WORK EXPERIENCE</p>
+        <p>year</p>
+      </div>
+      <div style={{ width: "50%" }}>
         <div>
-          <p>WORK EXPERIENCE</p>
-          <p>year</p>
-        </div>
-        <div style={{ width: "50%" }}>
-          <div>
-            <Flex
-              wrap="wrap"
-              gap="middle"
-              justify="space-between"
-              align="start"
-            >
-              <div>
-                <h4>Frontend dev</h4>
-                <p>NY, Remote</p>
-                <p>
-                  Job <span>Dec 2022 - Dec 2023</span>
-                </p>
-              </div>
-              <Flex wrap="wrap" gap="middle" justify="end" align="start">
-                <EditOutlined />
-                <DeleteOutlined />
-              </Flex>
+          <Flex wrap="wrap" gap="middle" justify="space-between" align="start">
+            <div>
+              <h4>Frontend dev</h4>
+              <p>NY, Remote</p>
+              <p>
+                Job <span>Dec 2022 - Dec 2023</span>
+              </p>
+            </div>
+            <Flex wrap="wrap" gap="middle" justify="end" align="start">
+              <EditOutlined />
+              <DeleteOutlined />
             </Flex>
-          </div>
-          <div>
-            <Button type="link">
-              <PlusOutlined />
-              Add Job
-            </Button>
-          </div>
+          </Flex>
         </div>
-      </Flex>
+        <div>
+          <Button type="link" onClick={() => setOpen(true)}>
+            <PlusOutlined />
+            Add Job
+          </Button>
+        </div>
+        <GlobalModal open={open} setOpen={setOpen} width={650} title={""}>
+          <ExperienceModal></ExperienceModal>
+        </GlobalModal>
+      </div>
+    </Flex>
   );
 };
 
