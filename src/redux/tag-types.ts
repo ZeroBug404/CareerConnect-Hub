4a441f0ac0cdbb3b1efb4869973acf8b9a4a2d20
shapna28 <shapna088@gmail.com>
2023-12-29 23:52:25 +0600

    added some resume api page

--- src/redux/tag-types.ts
+++ src/redux/tag-types.ts
@@ -2,6 +2,10 @@ export enum tagTypes {
   user = "user",
   jobs = "jobs",
   company = "company",
+  education= 'education',
+  workExperience = 'workExperience',
+  jobResponsibility = 'jobResponsibilities',
+  training = 'training',
 }
 
 export const tagTypesList = [tagTypes.user, tagTypes.jobs, tagTypes.company];
