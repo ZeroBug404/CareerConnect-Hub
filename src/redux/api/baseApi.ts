81fa7e6b2bd8a954e8f8b2c9d95eb2b14bc5c4f9
maruf1996 <maruf3813@gmail.com>
2024-01-02 21:23:54 +0600

    jobDetails-added

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { tagTypesList } from "../tag-types";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://career-connect-hub-api.vercel.app/api/v1",
+    baseUrl: "https://career-connect-hub-api-five.vercel.app/api/v1",
     // baseUrl: "http://localhost:5000/api/v1/",
   }),
   tagTypes: tagTypesList,
