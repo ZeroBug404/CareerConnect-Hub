d3bbd2cbb3cd837432a480f3f48a13acf021b4a4
Rabbi-Reza <rabbi.cse1.bu@gmail.com>
2024-01-02 11:45:38 +0600

    FAQ and Filter work

--- src/components/ui/JobListMain.tsx
+++ src/components/ui/JobListMain.tsx
@@ -5,19 +5,24 @@
 import styles from "@/Styles/JobList.module.css";
 import JobDetailView from "@/components/ui/JobDetailView";
 import JobLists from "@/components/ui/JobLists";
-import { baseApi } from "@/redux/api/baseApi";
-import { Button, Input, Space } from "antd"; // Import Spin from Ant Design
+import { DownOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Button, Dropdown, Input, Space, Typography } from "antd"; // Import Spin from Ant Design
 import { useEffect, useState } from "react";
 
 const JobListMain = async () => {
   const [selectedID, setSelectedID] = useState(null);
   const [allData, setAllData] = useState([]);
   const [singleData, setSingleData] = useState([]);
+  const [jobTypeValue, setJobTypeValue] = useState("");
+  const [experienceLevelValue, setExperienceLevelValue] = useState("");
 
   let search = "";
 
   const handleSearch = () => {
-    fetch(`https://career-connect-hub-api.vercel.app/api/v1/jobs?searchTerm=${search}`)
+    fetch(
+      `https://career-connect-hub-api.vercel.app/api/v1/jobs?searchTerm=${search}&jobType=${jobTypeValue}&experienceLevel=${experienceLevelValue}`
+    )
       .then((res) => res.json())
       .then((res) => {
         setAllData(res?.data?.data);
@@ -43,6 +48,51 @@ const JobListMain = async () => {
     handleSearch();
   }, []);
 
+  const JobTypeItems: MenuProps["items"] = [
+    {
+      label: "Full-Time",
+      key: "Full-time",
+    },
+    {
+      label: "Part-Time",
+      key: "Part-time",
+    },
+    {
+      label: "Contract",
+      key: "Contract",
+    },
+    {
+      label: "Freelance",
+      key: "Freelance",
+    },
+    {
+      label: "Select Job Type",
+      key: "",
+    },
+  ];
+
+  const experienceLevelItems: MenuProps["items"] = [
+    {
+      label: "Entry",
+      key: "Entry",
+    },
+    {
+      label: "Intermediate",
+      key: "Intermediate",
+    },
+    {
+      label: "Senior",
+      key: "Senior",
+    },
+  ];
+
+  const handleJobTypeFilter = (e: any) => {
+    setJobTypeValue(e.key);
+  };
+
+  const handleExperienceLevelFilter = (e: any) => {
+    setExperienceLevelValue(e.key);
+  };
   return (
     <div
       style={{
@@ -65,6 +115,38 @@ const JobListMain = async () => {
             Search
           </Button>
         </Space.Compact>
+        <Dropdown
+          menu={{
+            onClick: handleJobTypeFilter,
+            items: JobTypeItems,
+            selectable: true,
+            defaultSelectedKeys: [""],
+          }}
+        >
+          <Typography.Link>
+            <Space>
+              {jobTypeValue ? jobTypeValue : "Select Job Type"}
+
+              <DownOutlined />
+            </Space>
+          </Typography.Link>
+        </Dropdown>
+        <Dropdown
+          menu={{
+            onClick: handleExperienceLevelFilter,
+            items: experienceLevelItems,
+            selectable: true,
+            defaultSelectedKeys: [""],
+          }}
+        >
+          <Typography.Link>
+            <Space>
+              {experienceLevelValue ? experienceLevelValue : "Select Experience Level"}
+
+              <DownOutlined />
+            </Space>
+          </Typography.Link>
+        </Dropdown>
       </div>
       {allData.length > 0 ? (
         <div className={styles.JobList_div_main}>
