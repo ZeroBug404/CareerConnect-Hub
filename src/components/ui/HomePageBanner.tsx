94f2b7a743abfe60cddbe5bae5d16128b61f00df
shapna28 <shapna088@gmail.com>
2024-01-07 02:13:24 +0600

    done banner and footer design with antd

--- src/components/ui/HomePageBanner.tsx
+++ src/components/ui/HomePageBanner.tsx
@@ -12,67 +12,31 @@ const HomePageBanner = () => {
 
   return (
     <>
-      {/* <div className={styles.bannerHeading}>
-        <Row className={styles.bannerRow}>
-          <Col
-            xs={24}
-            sm={24}
-            md={24}
-            lg={12}
-            xl={12}
-            className={styles.bannerFirstCol}
-          >
-            <p className={styles.bannerText1} style={{ color: "#93aaec" }}>
-              CareerConnect Hub FOR EMPLOYERS
-            </p>
-            <h1 className="text-black">
-              Find Your Next Job with CareerConnect Hub
-            </h1>
-            <p className={styles.bannerText2}>
-              No matter the skills, experience, or qualifications you’re looking
-              for, you’ll find the right people on CareerConnect Hub’s matching
-              and hiring platform.
-            </p>
-            <Button
-              onClick={() => setOpen(true)}
-              className={styles.bannerButton}
-            >
-              Post a Job
-            </Button>
-          </Col>
-          <Col xs={24} sm={24} md={24} lg={12} xl={12}>
-            <img
-              src="/assets/hero-banner.webp"
-              alt="Banner Image"
-              className={styles.bannerImage}
-            />
-          </Col>
-        </Row>
-
-        <GlobalModal
-          open={open}
-          setOpen={setOpen}
-          width={600}
-          title={"Let's create your account"}
-        >
-          {" "}
-          <div className="">
-            <HomePageModal></HomePageModal>
-          </div>
-        </GlobalModal>
-      </div> */}
-      
       <Row
         justify="center"
         align="middle"
-        className="px-4 bg-[#123770] text-white min-h-screen"
+        style={{
+          padding: "0 20px",
+          color: "white",
+          backgroundColor: "#123770",
+        }}
       >
-        <Col xs={24} sm={12} data-aos="zoom-in-right" data-aos-duration="3000">
+        <Col xs={24} sm={12} data-aos="zoom-in-right" data-aos-duration="1000">
           <p>CareerConnect Hub FOR EMPLOYERS</p>
-          <h1 className="lg:text-4xl font-bold text-xl py-5 ">
+          <h1
+            style={{
+              fontSize: "30px",
+              padding: "20px 0",
+            }}
+          >
             Find Your Next Job with CareerConnect Hub
           </h1>
-          <p className="pb-5 lg:text-lg">
+          <p
+            style={{
+              fontSize: "18px",
+              paddingBottom: "20px",
+            }}
+          >
             No matter the skills, experience, or qualifications you’re looking
             for, you’ll find the right people on CareerConnect Hub’s matching
             and hiring platform.
@@ -81,8 +45,14 @@ const HomePageBanner = () => {
             Post a Job
           </Button>
         </Col>
-        <Col xs={24} sm={12} data-aos="zoom-in-left" data-aos-duration="3000">
-          <img src="/assets/home1.png" alt="Banner Image" className="w-full" />
+        <Col xs={24} sm={12} data-aos="zoom-in-left" data-aos-duration="1000">
+          <img
+            style={{
+              width: "100%",
+            }}
+            src="/assets/home1.png"
+            alt="Banner Image"
+          />
         </Col>
         <GlobalModal
           open={open}
@@ -90,7 +60,6 @@ const HomePageBanner = () => {
           width={600}
           title={"Let's create your account"}
         >
-          {" "}
           <div className="">
             <HomePageModal></HomePageModal>
           </div>
